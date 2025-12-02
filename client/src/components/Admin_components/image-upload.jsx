e7928ab248a38667a92955258426cc6c0021915d
aliftareq <aliftareq@gmail.com>
2025-12-02 23:28:44 +0600

    all api endpoints for admin created

--- client/src/components/Admin_components/image-upload.jsx
+++ client/src/components/Admin_components/image-upload.jsx
@@ -12,7 +12,7 @@ const productImageUpload = ({
   setImageFile,
   // uploadedImageUrl,
   setUploadedImageUrl,
-  setImageLoadingState
+  setImageLoadingState,
 }) => {
   const inputRef = useRef(null);
 
@@ -24,21 +24,21 @@ const productImageUpload = ({
     if (selectedFile) setImageFile(selectedFile);
   };
 
-  const handleDragOver = (event)=> {
+  const handleDragOver = (event) => {
     event.preventDefault();
-  }
+  };
   const handleDrop = (event) => {
     event.preventDefault();
     const droppedFile = event.dataTransfer.files?.[0];
     if (droppedFile) setImageFile(droppedFile);
-  }
+  };
 
-   const handleRemoveImage = () => {
+  const handleRemoveImage = () => {
     setImageFile(null);
     if (inputRef.current) {
       inputRef.current.value = "";
     }
-  }
+  };
 
   async function uploadImageToCloudinary() {
     setImageLoadingState(true);
@@ -56,22 +56,17 @@ const productImageUpload = ({
     }
   }
 
-
-   useEffect(() => {
+  useEffect(() => {
     if (imageFile !== null) uploadImageToCloudinary();
   }, [imageFile]);
 
   return (
-    <div
-    // className={`w-full  mt-4 ${isCustomStyling ? "" : "max-w-md mx-auto"}`}
-    >
-      <Label className="text-lg font-semibold mb-2 block">Upload Image</Label>
+    <div className="w-full mt-4 max-w-md mx-auto">
+      <Label className="text-lg font-semibold m-2 block text-center">Upload Image</Label>
       <div
-         onDragOver={handleDragOver}
-      onDrop={handleDrop}
-      // className={`${
-      //   isEditMode ? "opacity-60" : ""
-      // } border-2 border-dashed rounded-lg p-4`}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+        className="border-2 border-dashed rounded-lg p-4 mx-4"
       >
         <Input
           id="image-upload"
@@ -79,14 +74,11 @@ const productImageUpload = ({
           className="hidden"
           ref={inputRef}
           onChange={handleImageFileChange}
-          //   disabled={isEditMode}
         />
         {!imageFile ? (
           <Label
             htmlFor="image-upload"
-            // className={`${
-            //   isEditMode ? "cursor-not-allowed" : ""
-            // } flex flex-col items-center justify-center h-32 cursor-pointer`}
+            className={`flex flex-col items-center justify-center h-32 cursor-pointer`}
           >
             <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
             <span>Drag & drop or click to upload image</span>
@@ -96,8 +88,8 @@ const productImageUpload = ({
             <div className="flex items-center">
               <FileIcon className="w-8 h-8 text-primary mr-2 " />
             </div>
-             <p className="text-sm font-medium">{imageFile.name}</p>
-             <Button
+            <p className="text-sm font-medium">{imageFile.name}</p>
+            <Button
               variant="ghost"
               size="icon"
               className="text-muted-foreground hover:text-foreground"
--- server/routes/admin/products-routes.js
+++ server/routes/admin/products-routes.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
-
-import { handleImageUpload } from "../../controllers/admin/products-controller.js";
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  fetchAllProducts,
+  handleImageUpload,
+} from "../../controllers/admin/products-controller.js";
 
 import { upload } from "../../helpers/cloudinary.js";
-
 
 const router = Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+router.post("/add", addProduct);
+router.put("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
+router.get("/get", fetchAllProducts);
 
-
-export default router;  
+export default router;
