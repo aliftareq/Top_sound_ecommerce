91e3a012e8b1666c4e52635fbbfab81e8531b3a8
aliftareq <aliftareq@gmail.com>
2025-12-09 16:01:39 +0600

    shopping header page responsive navbar implemented

--- client/src/components/Shopping_components/header.jsx
+++ client/src/components/Shopping_components/header.jsx
@@ -1,9 +1,166 @@
-import React from 'react'
+import { useState } from "react";
+import { HousePlug, LogOut, Menu, ShoppingCart, UserCog } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { useDispatch, useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "@/config";
+import { Label } from "../ui/label";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { logoutUser } from "@/store/auth_slice";
 
-function ShoppingHeader() {
+const MenuItems = () => {
+  // const navigate = useNavigate();
+  // const location = useLocation();
+  // const [searchParams, setSearchParams] = useSearchParams();
+
+  // function handleNavigate(getCurrentMenuItem) {
+  //   sessionStorage.removeItem("filters");
+  //   const currentFilter =
+  //     getCurrentMenuItem.id !== "home" &&
+  //     getCurrentMenuItem.id !== "products" &&
+  //     getCurrentMenuItem.id !== "search"
+  //       ? {
+  //           category: [getCurrentMenuItem.id],
+  //         }
+  //       : null;
+
+  //   sessionStorage.setItem("filters", JSON.stringify(currentFilter));
+
+  //   location.pathname.includes("listing") && currentFilter !== null
+  //     ? setSearchParams(
+  //         new URLSearchParams(`?category=${getCurrentMenuItem.id}`)
+  //       )
+  //     : navigate(getCurrentMenuItem.path);
+  // }
+
   return (
-    <div>ShoppingHeader</div>
-  )
-}
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row pl-6 pt-6 lg:p-0">
+      {shoppingViewHeaderMenuItems.map((menuItem) => (
+        <Label
+          // onClick={() => handleNavigate(menuItem)}
+          className="text-sm font-medium cursor-pointer hover:underline"
+          key={menuItem.id}
+        >
+          {menuItem.label}
+        </Label>
+      ))}
+    </nav>
+  );
+};
 
-export default ShoppingHeader
+const HeaderRightContent = () => {
+  const { user } = useSelector((state) => state.auth);
+  // const { cartItems } = useSelector((state) => state.shopCart);
+  const [openCartSheet, setOpenCartSheet] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
+  // useEffect(() => {
+  //   dispatch(fetchCartItems(user?.id));
+  // }, [dispatch]);
+
+  // console.log(cartItems, "sangam");
+
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4 pl-6">
+      <Sheet open={openCartSheet} onOpenChange={() => setOpenCartSheet(false)}>
+        <Button
+          onClick={() => setOpenCartSheet(true)}
+          variant="outline"
+          size="icon"
+          className="relative"
+        >
+          <ShoppingCart className="w-6 h-6" />
+          <span className="absolute top-[-5px] right-0.5 font-bold text-sm">
+            {/* {cartItems?.items?.length || 0} */}0
+          </span>
+          <span className="sr-only">User cart</span>
+        </Button>
+        {/* <UserCartWrapper
+          setOpenCartSheet={setOpenCartSheet}
+          cartItems={
+            cartItems && cartItems.items && cartItems.items.length > 0
+              ? cartItems.items
+              : []
+          }
+        /> */}
+      </Sheet>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="bg-black">
+            <AvatarFallback className="bg-black text-white font-extrabold">
+              {user?.userName[0].toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="right" className="w-56 bg-white">
+          <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={() => navigate("/shop/account")}
+            className="cursor-pointer hover:underline"
+          >
+            <UserCog className="mr-2 h-4 w-4" />
+            Account
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={handleLogout}
+            className="cursor-pointer hover:underline"
+          >
+            <LogOut className="mr-2 h-4 w-4" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+const ShoppingHeader = () => {
+  //  const { isAuthenticated } = useSelector((state) => state.auth);
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+            <HeaderRightContent />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+        <div className="hidden lg:block">
+          <HeaderRightContent />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default ShoppingHeader;
