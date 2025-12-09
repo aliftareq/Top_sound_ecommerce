3a3b63df79e2221fa08f523368e8ebc3475836f8
aliftareq <aliftareq@gmail.com>
2025-12-09 23:54:40 +0600

    listing page layout and structure implemented

--- client/src/pages/Shopping_view/listing.jsx
+++ client/src/pages/Shopping_view/listing.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useState } from "react";
+import ProductFilter from "@/components/Shopping_components/filter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowUpDownIcon } from "lucide-react";
+import { sortOptions } from "@/config";
 
 function ShoppingListing() {
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState(null);
+
+  const handleSort = (value) => {
+    setSort(value);
+  };
+
+  const handleFilter = () => {
+    console.log("filter");
+  };
+
   return (
-    <div>Shopping view listing</div>
-  )
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+      <ProductFilter filters={filters} handleFilter={handleFilter} />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold">All Products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-slate-900 font-extrabold">
+              {/* {productList?.length} Prodfucts */}
+              Total Products : 10
+            </span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDownIcon className="h-4 w-4" />
+                  <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px] bg-white">
+                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem
+                      value={sortItem.id}
+                      key={sortItem.id}
+                      className="hover:cursor-pointer hover:underline"
+                    >
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ShoppingListing
+export default ShoppingListing;
