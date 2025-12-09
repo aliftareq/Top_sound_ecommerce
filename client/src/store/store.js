dd4593c8c79efa621ccb023bff0fd533cbfd1d15
aliftareq <aliftareq@gmail.com>
2025-12-10 01:00:22 +0600

    store hook created for filtered products

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,10 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth_slice";
 import adminProductsSlice from "./admin/products-slice";
 
+import shopProductsSlice from "./shop/products-slice";
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductsSlice,
+
+    shopProducts: shopProductsSlice,
   },
 });
 
