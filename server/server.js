b3b170209b6d43397d754e4e986131ddfcb553d4
aliftareq <aliftareq@gmail.com>
2026-01-07 14:56:38 +0600

    api's from backend in search funtionality implemented

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ import shopProductsRouter from "./routes/shop/products-routes.js";
 import shopCartRouter from "./routes/shop/cart-routes.js";
 import shopAddressRouter from "./routes/shop/address-routes.js";
 import shopOrderRouter from "./routes/shop/order-routes.js";
+import shopSearchRouter from "./routes/shop/search-routes.js";
 
 dotenv.config();
 
@@ -56,6 +57,7 @@ app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
 
 //running server
 app.listen(PORT, () => console.log(`Server is runnig on port ${PORT}`));
